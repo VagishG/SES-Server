6cf2e53568862aa7d31ed3cb1679c6eeff2fb222
VagishG <vagish.study@gmail.com>
2024-10-04 21:51:48 +0530

    Handling basic endpoints

--- src/controller/handleBounce.ts
+++ src/controller/handleBounce.ts
@@ -1,13 +1,44 @@
 import { prisma } from "../db";
 import type { Request, Response } from "express";
+import ses from "../helper/ses";
+import { SendEmailCommand } from "@aws-sdk/client-ses";
 
 const handleBounce = async (req: Request, res: Response): Promise<void> => {
   const { eventType, bounce } = req.body;
+  const params = {
+    Destination: {
+      ToAddresses: ["aayanguptastudent@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `${JSON.stringify(req.body)}`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "This is sample body",
+      },
+    },
+    ReplyToAddresses: ["support@avyuktsolutions.com"],
+    Source: "sales@avyuktsolutions.com",
+  };
 
-  if (!eventType || !bounce || eventType !== "Bounce") {
-    res.status(400).json({ message: "Invalid or missing event type" });
-    return;
+  try {
+    // await ses.sendEmail(params).promise();
+    const command = new SendEmailCommand(params);
+    await ses.send(command);
+
+    // const hashedOtp = await hashString(otp);
+    // console.log(hashedOtp);
+  } catch (err) {
+    console.error(err);
   }
+  // if (!eventType || !bounce || eventType !== "Bounce") {
+  //   res.status(400).json({ message: "Invalid or missing event type" });
+  //   return;
+  // }
 
   const { bouncedRecipients } = bounce;
 
