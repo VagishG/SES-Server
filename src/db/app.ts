6295da69eeac609d2a63404164c41b4d328d185e
VagishG <vagish.study@gmail.com>
2024-10-16 21:51:38 +0530

    Added cors

--- src/db/app.ts
+++ src/db/app.ts
@@ -1,8 +1,18 @@
 /** @format */
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// List of allowed origins
+const allowedOrigins = ["http://localhost:3000", "https://www.avyuktsolutions.com/"];
+
+// CORS options to allow multiple origins
+const corsOptions = {
+    origin: allowedOrigins, // Directly passing array of allowed origins
+    methods: "GET,POST", // Allow only specific methods
+};
 
 // Middleware to parse JSON bodies
 app.use(express.text({ type: 'text/plain' }));
@@ -11,11 +21,13 @@ app.use(express.json());
 // Static public folder
 app.use(express.static("public"));
 
+// Applying CORS to specific routes
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-import GlobalRouter from "../routes/global.routes"
-app.use("/api/v1", GlobalRouter);
+// Apply CORS to /api/v1 routes only with multiple origins
+import GlobalRouter from "../routes/global.routes";
+app.use("/api/v1", cors(corsOptions), GlobalRouter);
 
 export default app;
